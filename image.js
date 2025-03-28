aa6ecf039ed977a3e1fc69e0b386ea323992184b
naveencoding75 <naveensharmathor@gmail.com>
2025-03-28 21:19:34 +0530

    Initial commit: Added Image-Slider project

---
+++ image.js
@@ -0,0 +1,53 @@
+const total = document.querySelectorAll('.Image').length;
+const styleChange = document.querySelector(".Images");
+let i = 0;
+styleChange.style.width =  `${total * 100}vw`;
+updateButton();
+
+function slideLeft(){
+    if(i <= 0)return;
+    i--;
+    styleChange.style.transform = `translateX(${-100 * i}vw)`;
+    console.log("Current Index:", i);
+    updateButton();
+}
+
+function slideRight(){
+    if(i >= total - 1)return;
+    i++;
+    styleChange.style.transform = `translateX(${-100 * i}vw)`;
+    console.log("Current Index:", i);
+    updateButton();
+}
+
+function updateButton(){
+    document.querySelector('.left-button').style.display = i === 0 ? "none" : "block";
+    document.querySelector('.right-button').style.display = i === total - 1 ? "none" : "block";
+}
+
+let isAutoSliding = false;
+let intervalId;
+
+function automatic(){
+    if(!isAutoSliding){
+        lastCheck();
+        document.querySelector('.auto').innerHTML = `Stop`;
+        intervalId = setInterval(()=>{
+            lastCheck();
+            slideRight();
+        }, 1000);
+        isAutoSliding = true;
+    }else{
+        document.querySelector('.auto').innerHTML = `Auto Slide`;
+        clearInterval(intervalId);
+        isAutoSliding = false;
+    }
+}
+
+function lastCheck(){
+    if(i >= total - 1){
+        document.querySelector('.auto').innerHTML = `Auto Slide`;
+        clearInterval(intervalId);
+        isAutoSliding = false;
+    }
+}
