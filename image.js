22540d1c9a50e5118a7c5f653f2b6d747422e7f9
naveencoding75 <naveensharmathor@gmail.com>
2025-04-10 23:05:08 +0530

    fixed the buttons function

--- image.js
+++ image.js
@@ -1,69 +1,63 @@
 const total = document.querySelectorAll('.Image').length;
 const styleChange = document.querySelector(".Images");
 let i = 0;
-styleChange.style.width = `${total * 100}vw`;
+styleChange.style.width =  `${total * 100}vw`;
 updateButton();
 
-let isAutoSliding = false;
-let intervalId;
+let manual = false;
 
-function slideLeft() {
-    if (i <= 0) return;
+function manualCheck(){
+    manual = true;
+    document.querySelector('.auto').innerHTML = `Auto Slide`;
+    clearInterval(intervalId);
+    isAutoSliding = false;
+}
+
+function slideLeft(){
+    if(i <= 0)return;
     i--;
     styleChange.style.transform = `translateX(${-100 * i}vw)`;
     console.log("Current Index:", i);
     updateButton();
 }
 
-function slideRight() {
-    if (i >= total - 1) return;
+function slideRight(){
+    if(i >= total - 1)return;
     i++;
     styleChange.style.transform = `translateX(${-100 * i}vw)`;
     console.log("Current Index:", i);
     updateButton();
 }
 
-function updateButton() {
+function updateButton(){
     document.querySelector('.left-button').style.display = i === 0 ? "none" : "block";
     document.querySelector('.right-button').style.display = i === total - 1 ? "none" : "block";
 }
 
-function startAutoSlide() {
-    document.querySelector('.auto').innerHTML = `Stop`;
-    isAutoSliding = true;
-    intervalId = setInterval(() => {
-        if (i < total - 1) {
-            i++;
-            styleChange.style.transform = `translateX(${-100 * i}vw)`;
-            console.log("Current Index:", i);
-            updateButton();
-        } else {
-            stopAutoSlide();
-        }
-    }, 1000);
-}
+let isAutoSliding = false;
+let intervalId;
 
-function stopAutoSlide() {
-    document.querySelector('.auto').innerHTML = `Auto Slide`;
-    clearInterval(intervalId);
-    isAutoSliding = false;
-}
-
-function automatic() {
-    if (!isAutoSliding) {
-        startAutoSlide();
-    } else {
-        stopAutoSlide();
+function automatic(){
+    manual = false;
+    if(!isAutoSliding){
+        lastCheck();
+        document.querySelector('.auto').innerHTML = `Stop`;
+        intervalId = setInterval(()=>{
+            lastCheck();
+            slideRight();
+        }, 1000);
+        isAutoSliding = true;
+    }else{
+        document.querySelector('.auto').innerHTML = `Auto Slide`;
+        clearInterval(intervalId);
+        isAutoSliding = false;
     }
 }
 
-// Add event listeners to stop auto-slide when manual buttons are clicked
-document.querySelector('.left').addEventListener('click', () => {
-    if (isAutoSliding) stopAutoSlide();
-    slideLeft();
-});
-
-document.querySelector('.right').addEventListener('click', () => {
-    if (isAutoSliding) stopAutoSlide();
-    slideRight();
-});
+function lastCheck(){
+    if(i >= total - 1){
+        document.querySelector('.auto').innerHTML = `Auto Slide`;
+        clearInterval(intervalId);
+        isAutoSliding = false;
+    }
+}
